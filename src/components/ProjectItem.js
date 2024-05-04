cb5d22aca1f90f6df414e9b66d0dc61e961a736b
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-05-04 16:49:07 +0300

    "Render project technologies in ProjectItem component"
    This commit updates the ProjectItem component to map over the technologies array and render a span for each technology. The ProjectList component is also updated to map over the projects array and pass the necessary props

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const techLi = technologies.map((tech) => (
+    <span key={tech}>{tech}</span>
+  ));
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techLi}</div>
     </div>
   );
 }
-
+ 
 export default ProjectItem;
